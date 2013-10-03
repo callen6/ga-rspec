5b9710326b589639a933c9b8426072e5cfcacc13
Cori Allen <coriallen@gmail.com>
2013-10-03 11:29:07 -0400

    Adds square method and tests.

---
+++ lib/math.rb
@@ -0,0 +1,4 @@
+def square(x)
+	x * x
+	# or x ** 2
+end
--- spec/math_spec.rb
+++ spec/math_spec.rb
@@ -1,17 +1,21 @@
 require 'spec_helper'
 require_relative '../lib/math'
 
-describe '#square' do 
+describe '#square' do # use the # symbol because it indicates it's an instance method
 	it "should have a method called square" do
 	expect(method(:square)) # if :square weren't a symbol, test would run the method square
-end
+	end
 
-describe '#square' do # use the # symbol because it indicates it's an instance method
 	it "should have one required parameter called x" do
 		parameters = method(:square).parameters
 		expect(parameters[0]).to include (:req)
-		expect(paramerts[0]).to include(:x)
+		expect(parameters[0]).to include(:x)
 		expect(parameters.length).to eq 1
 	end
-	it 
+
+	it "should square numbers" do
+		expect(square(0.5)).to eq 0.25
+		# keep writing on own
+	end
+
 end
